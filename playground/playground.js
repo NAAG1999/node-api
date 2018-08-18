630061b3e3fb06f93c4c9e08e15645ac6bb82851
Nilesh <agnilesh8199@gmail.com>
2018-08-18 09:50:13 +0530

    clean up

--- playground/playground.js
+++ playground/playground.js
@@ -6,7 +6,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', function(err, client){
     }
     console.log('Connected to MongoDB server');
 
-    var db = client.db('testingFirst');
+    var db = client.db('Users');
 
     db.collection('Users').insertOne({
         Name: 'Nilesh',
@@ -15,9 +15,9 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', function(err, client){
     }, function(err,result){
         if(err){
             console.log('Unable to connect to the mongoDB');
-        }
-            console.log(result.ops);  /*OPS attribute is gonna store all the attributes of the documents*/ 
-    
-        client.close();
+           }
+            console.log(result.ops, undefined, 2);  /*OPS attribute is gonna store all the attributes of the documents*/ 
+           
+           client.close();
     });
 });
