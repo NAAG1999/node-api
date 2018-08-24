818f024b1004efa601d26b0ab617e57ae0218238
nil3sh99 <agnilesh8199@gmail.com>
2018-08-24 08:17:21 +0530

    deletions in mongodb

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -8,7 +8,12 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', function(err, client){
 
     var db = client.db('TodoApp'); //name of the database
 
-    db.collection('Users').find({name: 'nilesh'}).toArray().then((docs) => {
-    console.log(JSON.stringify(docs, undefined,  2));
+    db
+    .collection('Todos')
+    .deleteMany({text: 'Your text here from the database'})
+    .then((result) =>{
+        console.log(result);
     });
+    // there are other 2 methods too... 
+    // Best is to use findOneDelete method, as it returns the deleted value too
 });
