52467253c350da5d765d2119f26d25de8b5e9fc4
nil3sh99 <agnilesh8199@gmail.com>
2018-08-24 08:33:51 +0530

    extracting the MongoClient and ObjectID prop from mongoDB

---
+++ playground/mongodb-update.js
@@ -0,0 +1,15 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', function(err, client){
+    if(err){
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    var db = client.db('TodoApp'); //name of the database
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID
+    })
+});
