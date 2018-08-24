1d8b52da491faebec1e7578a76675b61a9ff05e7
nil3sh99 <agnilesh8199@gmail.com>
2018-08-24 08:53:40 +0530

    correction adding inc operator in js file for database

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -14,12 +14,12 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', function(err, client){
     },{
         $set: {
             name: "Nilesh"
-        }
-    },{ 
+        }, 
         $inc:{
             age: 1
         }
-    },{
+    },
+    {
         returnOriginal: false
     }).then((result) => {
         console.log(result);
