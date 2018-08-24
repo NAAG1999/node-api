1f771868c1da4ebc87eecd21201fccdd5cffcb57
nil3sh99 <agnilesh8199@gmail.com>
2018-08-24 08:19:45 +0530

    clean up

--- playground/mongodb-delete.js
+++ playground/mongodb-delete.js
@@ -15,5 +15,5 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', function(err, client){
         console.log(result);
     });
     // there are other 2 methods too... 
-    // Best is to use findOneDelete method, as it returns the deleted value too
+    // Best is to use findOneAndDelete method, as it returns the deleted value too
 });
